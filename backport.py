eea367d9b44379f047fdc48206d5bd576f9f7f32
DanielZh <haveacar.zhovnir@gmail.com>
2024-01-17 22:11:50 +0200

    update readme.md/003

--- backport.py
+++ backport.py
@@ -1,6 +1,6 @@
 import sys
 from controls import Backporter
-# files names
+
 RESULT_FILE_NAME = 'result.c'
 LOG_FILE_NAME = 'log.txt'
 GREEN = '\u001b[32m'
@@ -10,10 +10,14 @@ RESET = '\u001b[0m'
 def main(before_file: str, after_file: str, target_file: str):
     backporter = Backporter(before_file, after_file, target_file)
     result, log = backporter.apply_patch()
+
     backporter.write_file(RESULT_FILE_NAME, result)
     backporter.event(f'INFO Backporting completed. Check {RESULT_FILE_NAME}')
     backporter.write_file(LOG_FILE_NAME, log)
-    print(f"{GREEN}***Backporting completed. Check '{RESULT_FILE_NAME}' and '{LOG_FILE_NAME}' for details.***{RESET}")
+
+
+    print(f"{GREEN}***Backporting completed. Check '{RESULT_FILE_NAME}' and "
+          f"'{LOG_FILE_NAME}' for details.***{RESET}")
 
 
 if __name__ == "__main__":
--- controls.py
+++ controls.py
@@ -27,7 +27,6 @@ def find_file_in_root_then_dirs(filename: str) -> Optional[str]:
 
     return None
 
-
 class Backporter:
     def __init__(self, before_file: str, after_file: str, target_file: str) -> None:
         """
@@ -40,25 +39,24 @@ class Backporter:
         self._logs = []
         self.event('INFO started successfully.')
         try:
-            self.before = self._read_file(before_file)
-            self.after = self._read_file(after_file)
-            self.target = self._read_file(target_file)
+            self.before = list(self._read_file(before_file))
+            self.after = list(self._read_file(after_file))
+            self.target = list(self._read_file(target_file))
         except FileNotFoundError as e:
             self.write_file('log.txt', self._logs)
             print(e)
             sys.exit(1)
 
-    def _read_file(self, filename: str):
-        """Read a file and yield its lines one by one."""
+    def _read_file(self, filename: str) -> List[str]:
+        """Read a file and return its lines."""
         file_path = find_file_in_root_then_dirs(filename)
         if file_path is None:
-            text = f"ERROR File'{filename}' not found in project directory or subdirectories."
+            text = f"ERROR File '{filename}' not found in project directory or subdirectories."
             self.event(text)
             raise FileNotFoundError(text)
 
         with open(file_path, 'r') as file:
-            for line in file:
-                yield line
+            return file.readlines()
 
     def write_file(self, filename: str, data: List[str]) -> None:
         """
@@ -72,26 +70,11 @@ class Backporter:
 
     def event(self, event: str) -> None:
         timestamp = datetime.now()
-        event_string = f'{timestamp} {event} \n'
+        event_string = f'{timestamp} {event}\n'
         self._logs.append(event_string)
 
-    def create_patch(self):
-        before_lines = []
-        after_lines = []
-
-        try:
-            while True:
-                before_lines.append(next(self.before))
-        except StopIteration:
-            pass  # Exhausted all lines in self.before
-
-        try:
-            while True:
-                after_lines.append(next(self.after))
-        except StopIteration:
-            pass  # Exhausted all lines in self.after
-
-        return difflib.unified_diff(before_lines, after_lines, lineterm='')
+    def create_patch(self) -> List[str]:
+        return list(difflib.unified_diff(self.before, self.after, lineterm=''))
 
     def apply_patch(self) -> Tuple[List[str], List[str]]:
         patch_lines = self.create_patch()
@@ -100,24 +83,17 @@ class Backporter:
         i = 0
         for line in patch_lines:
             if line.startswith('@@'):
-                # Find the start line number
                 start_line = int(line.split()[1].split(',')[0][1:])
                 i = start_line - 1
             elif line.startswith('-'):
-                # Remove line from target
                 if i < len(result):
                     removed_line = result.pop(i)
                     self.event(f"INFO Line {i + 1} {removed_line.strip()} removed.")
-                else:
-                    i += 1  # Increment i only if the line wasn't removed
             elif line.startswith('+'):
-                # Add line to target
                 result.insert(i, line[1:])
                 self.event(f"INFO Line {i + 1} {line[1:].strip()} added.")
                 i += 1
             elif line.startswith(' '):
-                # Unchanged line
                 i += 1
 
         return result, self._logs
-
